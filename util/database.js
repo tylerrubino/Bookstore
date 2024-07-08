28ffaa0cea0957a6b215b2c6e4e79b2506498e1c
Tyler Rubino <tcdrubino@gmail.com>
2024-07-08 13:41:00 -0400

    added some functionality for mySQL backend functionality

--- util/database.js
+++ util/database.js
@@ -1,1 +1,10 @@
 const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	database: 'node-complete',
+	password: 'Honda1983!',
+});
+
+module.exports = pool.promise();
